e95ddb4b4ad8d639877ec4d237e18085becc94c7
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-27 18:50:42 +0100

    add migration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'HOME')
+
+@section('content')
+    <h1 class="my-3">HOME</h1>
+@endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,7 @@
 <header>
-
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand mx-2" href="{{ url('/') }}">
+            MyTRAINS
+        </a>
+    </nav>
 </header>
